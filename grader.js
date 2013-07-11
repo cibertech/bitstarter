58a7e53aa21a887264bb54f54cb4d53195d412f6
Ubuntu <ubuntu@ip-172-31-29-0.us-west-2.compute.internal>
2013-07-11 02:13:07 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -1,30 +1,9 @@
 #!/usr/bin/env node
-/*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var restler = require("restler");
+var rest = require("restler");
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -32,7 +11,7 @@ var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        process.exit(1);
     }
     return instr;
 };
@@ -57,15 +36,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 };
 
 var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
     return fn.bind({});
-};
-
-var gradeFile = function(file, checks) {
-  var checkJson = checkHtmlFile(file, checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
 };
 
 if(require.main == module) {
@@ -77,13 +48,13 @@ if(require.main == module) {
 
     if(program.url)
     {
-        restler.get(program.url).on(('complete'),
-		function(data)
-		{
-		    //TODO: This probably won't work with multiple grader.js running. 
-		    var tmpfile = "/tmp/grader";
-		    fs.writeFileSync(tmpfile, data);
-		    gradeFile(tmpfile, program.checks);
+        rest.get(program.url).on(('complete'), function(data)
+		{		    
+		    var out_file = "grader_output.txt";
+		    var checkJson = checkHtmlFile(program.file, program.checks);
+		    var outJson = JSON.stringify(checkJson, null, 4);
+		    console.log(outJson);
+		    fs.writeFileSync(out_file, outJson);
 		});
     }
     else
